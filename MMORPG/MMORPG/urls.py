6c7c7e1b31991de5afcb0e69ddc9048617ef9d81
Alex <vasilevskiysasha1@gmail.com>
2022-03-21 23:51:48 +0300

    Working ckeditor

--- MMORPG/MMORPG/urls.py
+++ MMORPG/MMORPG/urls.py
@@ -16,15 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('news.urls')),
 
-
     path('pages/', include('django.contrib.flatpages.urls')),
     path('', lambda request: redirect('home/', permanent=False), name='home_page'),
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
